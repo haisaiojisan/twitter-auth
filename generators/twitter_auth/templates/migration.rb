3e4d2910fbf28b8a5a46570e37d5f788cf15efe9
jugyo <jugyo.org@gmail.com>
2010-02-06 22:40:10 +0900

    fixed migration template

--- generators/twitter_auth/templates/migration.rb
+++ generators/twitter_auth/templates/migration.rb
@@ -17,23 +17,23 @@ class TwitterAuthMigration < ActiveRecord::Migration
       # This information is automatically kept
       # in-sync at each login of the user. You
       # may remove any/all of these columns.
-      t.string :name
-      t.string :location
-      t.string :description
-      t.string :profile_image_url
-      t.string :url
-      t.boolean :protected
-      t.string :profile_background_color
-      t.string :profile_sidebar_fill_color
-      t.string :profile_link_color
-      t.string :profile_sidebar_border_color
-      t.string :profile_text_color
-      t.string :profile_background_image_url
-      t.boolean :profile_background_tile
-      t.integer :friends_count
-      t.integer :statuses_count
-      t.integer :followers_count
-      t.integer :favourites_count
+      # t.string :name
+      # t.string :location
+      # t.string :description
+      # t.string :profile_image_url
+      # t.string :url
+      # t.boolean :protected
+      # t.string :profile_background_color
+      # t.string :profile_sidebar_fill_color
+      # t.string :profile_link_color
+      # t.string :profile_sidebar_border_color
+      # t.string :profile_text_color
+      # t.string :profile_background_image_url
+      # t.boolean :profile_background_tile
+      # t.integer :friends_count
+      # t.integer :statuses_count
+      # t.integer :followers_count
+      # t.integer :favourites_count
 
       # Probably don't need both, but they're here.
       t.integer :utc_offset
